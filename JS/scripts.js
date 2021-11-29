78dceb1ba23e5bebad87f19d8cd57a68453408da
matthewvolny <matthew.volny@gmail.com>
2021-11-28 23:59:00 -0500

    improved layout and design

--- JS/scripts.js
+++ JS/scripts.js
@@ -7,14 +7,14 @@ let completedTasks = document.querySelector(".completed-tasks-interface");
 const addTask = () => {
   let task = document.getElementById("text-box").value;
   let newTask = document.createElement("newTask");
-  newTask.className = "col-4 new-task";
+  newTask.className = "col-6 new-task margin";
   newTask.innerHTML = task;
   let checkMark = document.createElement("button");
   checkMark.textContent = "Completed?";
-  checkMark.className = "col-4 check-mark task-button";
+  checkMark.className = "col-3 check-mark task-button margin";
   let removeButton = document.createElement("button");
   removeButton.textContent = "Remove?";
-  removeButton.className = "col-4 remove-button task-button";
+  removeButton.className = "col-3 remove-button task-button margin";
   pendingTasks.appendChild(checkMark);
   pendingTasks.appendChild(newTask);
   pendingTasks.appendChild(removeButton);
@@ -30,11 +30,12 @@ const addTask = () => {
   const moveTaskToCompleted = () => {
     console.log("hello");
     let checkMarkClone = checkMark.cloneNode(true);
-    checkMarkClone.className = "col-4 check-mark-clone task-button";
+    checkMarkClone.className = "col-3 check-mark-clone task-button margin";
     let newTaskClone = newTask.cloneNode(true);
-    newTaskClone.className = "col-4 new-task-clone";
+    newTaskClone.className = "col-6 new-task-clone margin";
     let removeButtonClone = removeButton.cloneNode(true);
-    removeButtonClone.className = "col-4 remove-button-clone task-button";
+    removeButtonClone.className =
+      "col-3 remove-button-clone task-button margin";
     completedTasks.appendChild(checkMarkClone);
     completedTasks.appendChild(newTaskClone);
     completedTasks.appendChild(removeButtonClone);
@@ -53,11 +54,11 @@ const addTask = () => {
     const moveBackToPending = () => {
       console.log("hello");
       let checkMark = checkMarkClone.cloneNode(true);
-      checkMark.className = "col-4 check-mark task-button";
+      checkMark.className = "col-3 check-mark task-button margin";
       let newTask = newTaskClone.cloneNode(true);
-      newTask.className = "col-4 new-task";
+      newTask.className = "col-6 new-task margin";
       let removeButton = removeButtonClone.cloneNode(true);
-      removeButton.className = "col-4 remove-button task-button";
+      removeButton.className = "col-3 remove-button task-button margin";
       pendingTasks.appendChild(checkMark);
       pendingTasks.appendChild(newTask);
       pendingTasks.appendChild(removeButton);
